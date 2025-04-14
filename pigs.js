62997e77299c90893062b91ec5df4ffe4efbcecd
Ady823 <aduval@brooksschool.org>
2025-04-14 13:14:49 -0400

    handScore function edit

--- pigs.js
+++ pigs.js
@@ -10,8 +10,8 @@ let score2 = 0;
 let score3 = 0;
 let game = true;
 
-//function playerScoring() {
-    while(game) {
+function playerScoring() {
+    if(game) {
         if(score1 + handScore1 >= winningScore) {
            console.log("Player 0 wins");
            game = false;
@@ -29,7 +29,7 @@ let game = true;
             game = false;
       }
     }
-//}
+}
 
 function handleClick(id) {
     console.log(id);
@@ -39,24 +39,34 @@ function handleClick(id) {
     if(id.includes("Roll")) {
         rollClick(id)
     }
-   // playerScoring();
+    playerScoring();
 }
 
 function passClick(id) {
     if(id == "player0PassButton") {
         score0 += handScore0;
-       // player0HandScore.innerHTML = "Score: " + handScore2;
-       //TODO: do the total score function for all four players
+        handScore0 = 0;
+        player0HandScore.innerHTML = "Score: " + 0;
+        player0TotalScore.innerHTML = "Total Score: " + score0;
         currentPlayer = 1;
     } else if(id == "player1PassButton") {
         score1 += handScore1;
+        handScore1 = 0;
         currentPlayer = 2;
+        player1HandScore.innerHTML = "Score: " + 0;
+        player1TotalScore.innerHTML = "Total Score: " + score1;
     } else if(id == "player2PassButton") {
         score2 += handScore2;
+        handScore2 = 0;
         currentPlayer = 3;
+        player2HandScore.innerHTML = "Score: " + 0;
+        player2TotalScore.innerHTML = "Total Score: " + score2;
     } else if(id == "player3PassButton") {
         score3 += handScore3;
+        handScore3 = 0;
         currentPlayer = 0;
+        player3HandScore.innerHTML = "Score: " + 0;
+        player3TotalScore.innerHTML = "Total Score: " + score3;
     }
 }
 
@@ -73,6 +83,19 @@ function rollClick(id) {
     }
     console.log(currentPlayer)
     playerRoll(currentPlayer)
+
+    if(score0 >= 100) {
+        console.log("0 wins")
+    }
+    if(score1 >= 100) {
+        console.log("1 wins")
+    }
+    if(score2 >= 100) {
+        console.log("2 wins")
+    }
+    if(score3 >= 100) {
+        console.log("3 wins")
+    }
 }
 
 function playerRoll(player) {
@@ -115,16 +138,16 @@ function playerRoll(player) {
     setRoll(player, pig1, pig2);
 
     if(player == 0) {
-        handScore0 = handScore(handScore, pig1, pig2)
+        handScore0 = getHandScore(handScore0, pig1, pig2)
         player0HandScore.innerHTML = "Score: " + handScore0;
     } else if (player == 1) {
-        handScore1 = handScore(handScore, pig1, pig2)
+        handScore1 = getHandScore(handScore1, pig1, pig2)
         player1HandScore.innerHTML = "Score: " + handScore1;
     } else if (player == 2) {
-        handScore2 = handScore(handScore, pig1, pig2)
+        handScore2 = getHandScore(handScore2, pig1, pig2)
         player2HandScore.innerHTML = "Score: " + handScore2;
     } else {
-        handScore3 = handScore(handScore, pig1, pig2)
+        handScore3 = getHandScore(handScore3, pig1, pig2)
         player3HandScore.innerHTML = "Score: " + handScore3;
     }
 
@@ -174,37 +197,37 @@ function pigOut(pig1, pig2) {
     }
 }
 
-function handScore(handScore, pig1, pig2) {
+function getHandScore(handScore, pig1, pig2) {
     let points = 0;
-    if (pig1.equals("dot") && pig2.equals("no dot") || pig1.equals("no dot") && pig2.equals("dot")){
+    if (pig1 == "dot" && pig2 == "no dot" || pig1 == "no dot" && pig2 == "dot"){
         handScore = 0;
         return handScore;
     }
-    if (pig1.equals("dot") && pig2.equals("dot") ) {
-        points = 0.5; // will be multiplied by two later on
-    } else if(pig1.equals("no dot") && pig2.equals("no dot")) {
-        points = 0.5; 
+    if ((pig1 == "dot") && (pig2 == "dot")) {
+        points = 1; 
+    } else if (pig1 == "no dot" && pig2 == "no dot") {
+        points = 1;
     } else {
-        if (pig1.equals("razorback")) {
+        if (pig1 == "razorback") {
             points += 5;
-        } else if (pig1.equals("trotter")) {
+        } else if (pig1 == "trotter") {
             points += 5;
-        } else if (pig1.equals("snouter")) {
+        } else if (pig1 == "snouter") {
             points += 10;
-        } else if (pig1.equals("leaning jowler")) {
+        } else if (pig1 == "leaning jowler") {
             points += 15;
         }
-        if (pig2.equals("razorback")) {
+        if (pig2 == "razorback") {
             points += 5;
-        } else if (pig2.equals("trotter")) {
+        } else if (pig2 == "trotter") {
             points += 5;
-        } else if (pig2.equals("snouter")) {
+        } else if (pig2 == "snouter") {
             points += 10;
-        } else if (pig2.equals("leaning jowler")) {
+        } else if (pig2 == "leaning jowler") {
             points += 15;
         }
         
-        if (pig1.equals(pig2)){
+        if (pig1 == pig2){
             points = points * 2;
         }
     }
